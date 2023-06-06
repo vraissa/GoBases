e051b1608f4aa8722fe52187770d8732c433f6d9
Vitoria Raissa Zeferino <90735412+vraissa@users.noreply.github.com>
2023-06-06 18:00:37 -0300

    exemplo

---
+++ GoWeb/Exemplos/Group_POST/groupPost.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	ID         int     `json:"id"`
+	Nome       string  `json:"nome"`
+	Tipo       string  `json:"tipo"`
+	Quantidade int     `json:"quantidade"`
+	Preco      float64 `json:"preco"`
+}
+
+func Salvar() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var req request
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		req.ID = 4
+		c.JSON(http.StatusCreated, req)
+	}
+}
+
+func main() {
+	r := gin.Default()
+	pr := r.Group("/produtos")
+	pr.POST("/salvar", Salvar())
+	r.Run()
+}
